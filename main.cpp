82be0dafc571c62f18f9af23f9843034f20741da
antonsavchenko94 <anton@interlink-ua.com>
2014-05-27 15:10:58 +0300

    include header file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include "mainwindow.h"
-#include "email_address_retrieval_application.h"
+#include "Main_Window.h"
+#include "Email_Address_Retrieval_Application.h"
 
 int main(int argc, char *argv[]) {
     EmailAddressRetrievalApplication eara;
